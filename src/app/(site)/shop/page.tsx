6015f3066fc75b667bc0d44e932d14656b44e272
Gilang Aryo <gilang91668@gmail.com>
2025-10-20 00:47:33 +0700

    fix shop

--- src/app/(site)/shop/page.tsx
+++ src/app/(site)/shop/page.tsx
@@ -1,10 +1,12 @@
+"use client";
+
+import { Suspense } from "react";
 import CatalogSection from "../components/shop/CatalogSection";
 
-
-export default function Home() {
+export default function ShopPage() {
     return (
-        <>
+        <Suspense fallback={<div className="min-h-screen flex items-center justify-center">Loading...</div>}>
             <CatalogSection />
-        </>
+        </Suspense>
     );
 }
